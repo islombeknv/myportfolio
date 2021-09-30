3766d570ea2f62f7750a49a78c7696d60a3857f0
IslombekNv <islomjon2702@gmail.com>
2021-09-30 16:39:09 +0500

    update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -111,14 +111,6 @@ CKEDITOR_CONFIGS = {
     },
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = config('EMAIL_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
-
 try:
     from .settings_local import *
 except ImportError:
